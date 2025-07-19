6b3f3cf04d283d7c780f35c748bbe2dd3024b294
Carlos Moran <moran_krlos@hotmail.com>
2025-07-19 17:24:03 -0600

    form validations

--- front-end/src/app/destino/nuevo/components/add-destionation-form.tsx
+++ front-end/src/app/destino/nuevo/components/add-destionation-form.tsx
@@ -1,23 +1,52 @@
+"use client";
+
+import { apiUrl } from "@/contants/environment";
+import { useForm } from "react-hook-form";
+
 const styles = {
   label: "w-full text-left font-amiko text-xl leading-[35px]",
   labelContainer: "w-full flex flex-col items-center h-[52px] justify-center",
-  input: "w-full bg-page-background rounded-[10px] text-xl h-[52px] outline-none px-4"
+  input: "w-full bg-page-background rounded-[10px] text-xl h-[52px] outline-none px-4",
+  error: "text-red-500 text-sm mt-2"
 };
 
+interface DestinationForm {
+  nombre: string;
+  direccion: string;
+  descripcion: string;
+  url: string;
+}
+
 export default function AddDestinationForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<DestinationForm>();
+
+  const onSubmit = (data: DestinationForm) => {
+    console.log(JSON.stringify(data, null, 2), apiUrl);
+  };
+
   return (
-    <div className="bg-card-background rounded-[10px] pt-20 pb-44 mb-40 flex flex-col items-center">
-      <h1 className="font-amiko text-5xl mb-24">Agregar destino turistico</h1>
+    <div className="bg-card-background rounded-[10px] pt-20 pb-44 mb-40 px-4 flex flex-col items-center">
+      <h1 className="font-amiko text-3xl md:text-5xl mb-24">Agregar destino turistico</h1>
       <div className="max-w-[735px] w-full flex flex-col items-center">
         <div className={styles.labelContainer}>
           <label htmlFor="nombre" className={styles.label}>Nombre</label>
         </div>
-        <input id="nombre" className={styles.input} />
+        <input
+          id="nombre"
+          className={styles.input}
+          {...register("nombre", { required: "El nombre es requerido" })}
+        />
+        {errors.nombre && <span className={styles.error}>{errors.nombre.message}</span>}
 
         <div className={styles.labelContainer}>
           <label htmlFor="direccion" className={styles.label}>Dirección</label>
         </div>
-        <input id="direccion" className={styles.input} />
+        <input
+          id="direccion"
+          className={styles.input}
+          {...register("direccion", { required: "La dirección es requerida" })}
+        />
+        {errors.direccion && <span className={styles.error}>{errors.direccion.message}</span>}
 
         <div className={styles.labelContainer}>
           <label htmlFor="descripcion" className={styles.label}>Descripción</label>
@@ -26,14 +55,30 @@ export default function AddDestinationForm() {
           id="descripcion"
           className={`${styles.input} !h-[121px] py-4`}
           rows={2}
+          {...register("descripcion", { required: "La descripción es requerida" })}
         />
+        {errors.descripcion && <span className={styles.error}>{errors.descripcion.message}</span>}
 
         <div className={styles.labelContainer}>
           <label htmlFor="url" className={styles.label}>Url imágen</label>
         </div>
-        <input id="url" className={styles.input} />
+        <input
+          id="url"
+          className={styles.input}
+          {...register("url", {
+            required: "La URL es obligatoria",
+            pattern: {
+              value: /^(https?:\/\/)[\w.-]+(?:\.[\w\.-]+)+(?:[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+)?$/,
+              message: "Debe ser una URL válida que comience con http:// o https://"
+            }
+          })}
+        />
+        {errors.url && <span className={styles.error}>{errors.url.message}</span>}
 
-        <button className="font-abeezee bg-primary text-on-primary rounded-[5px] w-full max-w-[598px] h-[50px] shadow-xl cursor-pointer mt-24">
+        <button
+          className="font-abeezee bg-primary text-on-primary rounded-[5px] w-full max-w-[598px] h-[50px] shadow-xl cursor-pointer mt-24"
+          onClick={handleSubmit(onSubmit)}
+        >
           Agregar destino
         </button>
       </div>
